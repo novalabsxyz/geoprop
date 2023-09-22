dc8cae8e2733a902bc983921884e2d8795a1537e
Jay Kickliter <jay@kickliter.com>
2023-09-22 16:27:41 -0600

    [repo] switch to approx crate for comparisons

--- terrain/src/haversine.rs
+++ terrain/src/haversine.rs
@@ -9,30 +9,30 @@ use num_traits::FromPrimitive;
 
 /// Returns a new Point along a great circle route between two existing points
 pub(crate) trait HaversineIntermediate<T: CoordFloat> {
-    // Returns a new Point along a great circle route between two existing points.
-    //
-    // # Examples
-    //
-    // ```
-    // # use approx::assert_relative_eq;
-    // use geo::HaversineIntermediate;
-    // use geo::Point;
-    //
-    // let p1 = Point::new(10.0, 20.0);
-    // let p2 = Point::new(125.0, 25.0);
-    // let i20 = p1.haversine_intermediate(&p2, 0.2);
-    // let i50 = p1.haversine_intermediate(&p2, 0.5);
-    // let i80 = p1.haversine_intermediate(&p2, 0.8);
-    // let i20_should = Point::new(29.8, 29.9);
-    // let i50_should = Point::new(65.8, 37.7);
-    // let i80_should = Point::new(103.5, 33.5);
-    // assert_relative_eq!(i20.x(), i20_should.x(), epsilon = 0.2);
-    // assert_relative_eq!(i20.y(), i20_should.y(), epsilon = 0.2);
-    // assert_relative_eq!(i50.x(), i50_should.x(), epsilon = 0.2);
-    // assert_relative_eq!(i50.y(), i50_should.y(), epsilon = 0.2);
-    // assert_relative_eq!(i80.x(), i80_should.x(), epsilon = 0.2);
-    // assert_relative_eq!(i80.y(), i80_should.y(), epsilon = 0.2);
-    // ```
+    /// Returns a new Point along a great circle route between two existing points.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use approx::assert_relative_eq;
+    /// use geo::HaversineIntermediate;
+    /// use geo::Point;
+    ///
+    /// let p1 = Point::new(10.0, 20.0);
+    /// let p2 = Point::new(125.0, 25.0);
+    /// let i20 = p1.haversine_intermediate(&p2, 0.2);
+    /// let i50 = p1.haversine_intermediate(&p2, 0.5);
+    /// let i80 = p1.haversine_intermediate(&p2, 0.8);
+    /// let i20_should = Point::new(29.8, 29.9);
+    /// let i50_should = Point::new(65.8, 37.7);
+    /// let i80_should = Point::new(103.5, 33.5);
+    /// assert_relative_eq!(i20.x(), i20_should.x(), epsilon = 0.2);
+    /// assert_relative_eq!(i20.y(), i20_should.y(), epsilon = 0.2);
+    /// assert_relative_eq!(i50.x(), i50_should.x(), epsilon = 0.2);
+    /// assert_relative_eq!(i50.y(), i50_should.y(), epsilon = 0.2);
+    /// assert_relative_eq!(i80.x(), i80_should.x(), epsilon = 0.2);
+    /// assert_relative_eq!(i80.y(), i80_should.y(), epsilon = 0.2);
+    /// ```
 
     fn haversine_intermediate(&self, other: &Point<T>, f: T) -> Point<T>;
     fn haversine_intermediate_fill(
@@ -210,82 +210,82 @@ fn get_params<T: CoordFloat + FromPrimitive>(p1: &Point<T>, p2: &Point<T>) -> Ha
     }
 }
 
-// #[cfg(test)]
-// mod test {
-//     use super::*;
-//     use assert_approx_eq::assert_approx_eq;
+#[cfg(test)]
+mod test {
+    use super::{HaversineIntermediate, Point};
+    use approx::assert_relative_eq;
 
-//     #[test]
-//     fn f_is_zero_or_one_test() {
-//         let p1 = Point::new(10.0, 20.0);
-//         let p2 = Point::new(15.0, 25.0);
-//         let i0 = p1.haversine_intermediate(&p2, 0.0);
-//         let i100 = p1.haversine_intermediate(&p2, 1.0);
-//         assert_approx_eq!(i0.x(), p1.x(), 1.0e-6);
-//         assert_approx_eq!(i0.y(), p1.y(), 1.0e-6);
-//         assert_approx_eq!(i100.x(), p2.x(), 1.0e-6);
-//         assert_approx_eq!(i100.y(), p2.y(), 1.0e-6);
-//     }
+    #[test]
+    fn f_is_zero_or_one_test() {
+        let p1 = Point::new(10.0, 20.0);
+        let p2 = Point::new(15.0, 25.0);
+        let i0 = p1.haversine_intermediate(&p2, 0.0);
+        let i100 = p1.haversine_intermediate(&p2, 1.0);
+        assert_relative_eq!(i0.x(), p1.x(), epsilon = 1.0e-6);
+        assert_relative_eq!(i0.y(), p1.y(), epsilon = 1.0e-6);
+        assert_relative_eq!(i100.x(), p2.x(), epsilon = 1.0e-6);
+        assert_relative_eq!(i100.y(), p2.y(), epsilon = 1.0e-6);
+    }
 
-//     #[test]
-//     fn various_f_values_test() {
-//         let p1 = Point::new(10.0, 20.0);
-//         let p2 = Point::new(125.0, 25.0);
-//         let i20 = p1.haversine_intermediate(&p2, 0.2);
-//         let i50 = p1.haversine_intermediate(&p2, 0.5);
-//         let i80 = p1.haversine_intermediate(&p2, 0.8);
-//         let i20_should = Point::new(29.83519, 29.94841);
-//         let i50_should = Point::new(65.87471, 37.72201);
-//         let i80_should = Point::new(103.56036, 33.50518);
-//         assert_approx_eq!(i20.x(), i20_should.x(), 0.2);
-//         assert_approx_eq!(i20.y(), i20_should.y(), 0.2);
-//         assert_approx_eq!(i50.x(), i50_should.x(), 0.2);
-//         assert_approx_eq!(i50.y(), i50_should.y(), 0.2);
-//         assert_approx_eq!(i80.x(), i80_should.x(), 0.2);
-//         assert_approx_eq!(i80.y(), i80_should.y(), 0.2);
-//     }
+    #[test]
+    fn various_f_values_test() {
+        let p1 = Point::new(10.0, 20.0);
+        let p2 = Point::new(125.0, 25.0);
+        let i20 = p1.haversine_intermediate(&p2, 0.2);
+        let i50 = p1.haversine_intermediate(&p2, 0.5);
+        let i80 = p1.haversine_intermediate(&p2, 0.8);
+        let i20_should = Point::new(29.83519, 29.94841);
+        let i50_should = Point::new(65.87471, 37.72201);
+        let i80_should = Point::new(103.56036, 33.50518);
+        assert_relative_eq!(i20.x(), i20_should.x(), epsilon = 0.2);
+        assert_relative_eq!(i20.y(), i20_should.y(), epsilon = 0.2);
+        assert_relative_eq!(i50.x(), i50_should.x(), epsilon = 0.2);
+        assert_relative_eq!(i50.y(), i50_should.y(), epsilon = 0.2);
+        assert_relative_eq!(i80.x(), i80_should.x(), epsilon = 0.2);
+        assert_relative_eq!(i80.y(), i80_should.y(), epsilon = 0.2);
+    }
 
-//     #[test]
-//     fn should_be_north_pole_test() {
-//         let p1 = Point::new(0.0, 10.0);
-//         let p2 = Point::new(180.0, 10.0);
-//         let i50 = p1.haversine_intermediate(&p2, 0.5);
-//         let i50_should = Point::new(90.0, 90.0);
-//         assert_approx_eq!(i50.x(), i50_should.x(), 1.0e-6);
-//         assert_approx_eq!(i50.y(), i50_should.y(), 1.0e-6);
-//     }
+    #[test]
+    fn should_be_north_pole_test() {
+        let p1 = Point::new(0.0, 10.0);
+        let p2 = Point::new(180.0, 10.0);
+        let i50 = p1.haversine_intermediate(&p2, 0.5);
+        let i50_should = Point::new(90.0, 90.0);
+        assert_relative_eq!(i50.x(), i50_should.x(), epsilon = 1.0e-6);
+        assert_relative_eq!(i50.y(), i50_should.y(), epsilon = 1.0e-6);
+    }
 
-//     #[test]
-//     fn should_be_start_end_test() {
-//         let p1 = Point::new(30.0, 40.0);
-//         let p2 = Point::new(40.0, 50.0);
-//         let max_dist = 1500000.0; // meters
-//         let include_ends = true;
-//         let route = p1.haversine_intermediate_fill(&p2, max_dist, include_ends);
-//         assert_eq!(route, vec![p1, p2]);
-//     }
+    #[test]
+    fn should_be_start_end_test() {
+        let p1 = Point::new(30.0, 40.0);
+        let p2 = Point::new(40.0, 50.0);
+        let max_dist = 1500000.0; // meters
+        let include_ends = true;
+        let route = p1.haversine_intermediate_fill(&p2, max_dist, include_ends);
+        assert_eq!(route, vec![p1, p2]);
+    }
 
-//     #[test]
-//     fn should_add_i50_test() {
-//         let p1 = Point::new(30.0, 40.0);
-//         let p2 = Point::new(40.0, 50.0);
-//         let max_dist = 1000000.0; // meters
-//         let include_ends = true;
-//         let i50 = p1.clone().haversine_intermediate(&p2, 0.5);
-//         let route = p1.haversine_intermediate_fill(&p2, max_dist, include_ends);
-//         assert_eq!(route, vec![p1, i50, p2]);
-//     }
+    #[test]
+    fn should_add_i50_test() {
+        let p1 = Point::new(30.0, 40.0);
+        let p2 = Point::new(40.0, 50.0);
+        let max_dist = 1000000.0; // meters
+        let include_ends = true;
+        let i50 = p1.clone().haversine_intermediate(&p2, 0.5);
+        let route = p1.haversine_intermediate_fill(&p2, max_dist, include_ends);
+        assert_eq!(route, vec![p1, i50, p2]);
+    }
 
-//     #[test]
-//     fn should_add_i25_i50_i75_test() {
-//         let p1 = Point::new(30.0, 40.0);
-//         let p2 = Point::new(40.0, 50.0);
-//         let max_dist = 400000.0; // meters
-//         let include_ends = true;
-//         let i25 = p1.clone().haversine_intermediate(&p2, 0.25);
-//         let i50 = p1.clone().haversine_intermediate(&p2, 0.5);
-//         let i75 = p1.clone().haversine_intermediate(&p2, 0.75);
-//         let route = p1.haversine_intermediate_fill(&p2, max_dist, include_ends);
-//         assert_eq!(route, vec![p1, i25, i50, i75, p2]);
-//     }
-// }
+    #[test]
+    fn should_add_i25_i50_i75_test() {
+        let p1 = Point::new(30.0, 40.0);
+        let p2 = Point::new(40.0, 50.0);
+        let max_dist = 400000.0; // meters
+        let include_ends = true;
+        let i25 = p1.clone().haversine_intermediate(&p2, 0.25);
+        let i50 = p1.clone().haversine_intermediate(&p2, 0.5);
+        let i75 = p1.clone().haversine_intermediate(&p2, 0.75);
+        let route = p1.haversine_intermediate_fill(&p2, max_dist, include_ends);
+        assert_eq!(route, vec![p1, i25, i50, i75, p2]);
+    }
+}
