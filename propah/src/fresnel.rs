09df809db3e5ecf492c190bc4fbff9ed96cfbbad
Jay Kickliter <jay@kickliter.com>
2023-10-08 10:22:57 -0600

    [propah] impl fresnel zone

--- propah/src/fresnel.rs
+++ propah/src/fresnel.rs
@@ -1,5 +1,5 @@
 use num_traits::{AsPrimitive, Float};
-use std::{iter::Iterator, num::NonZeroU8, ops::Range};
+use std::{iter::Iterator, ops::Range};
 
 /// Speed of light in m/s
 const C: usize = 299_792_458;
@@ -8,34 +8,36 @@ const C: usize = 299_792_458;
 #[derive(Debug)]
 pub struct FresnelZone<T> {
     /// Which fresnel zone we're interested in.
-    _zone: NonZeroU8,
+    zone: u8,
     wavelength: T,
-    _distance_m: T,
+    distance_m: T,
 }
 
 impl<T> FresnelZone<T> {
     /// Returns a new FesnelZone object.
-    pub fn new(zone: NonZeroU8, f_hz: T, distance_m: T) -> Self
+    pub fn new(zone: u8, f_hz: T, distance_m: T) -> Self
     where
         T: Float + 'static,
         usize: AsPrimitive<T>,
     {
         let wavelength = C.as_() / f_hz;
         Self {
-            _zone: zone,
+            zone,
             wavelength,
-            _distance_m: distance_m,
+            distance_m,
         }
     }
 
     /// Returns a new FresnelZoneIter of length `len`.
     pub fn iter(&self, len: usize) -> FresnelZoneIter<T>
     where
-        T: Copy,
+        T: Copy + 'static,
+        u8: AsPrimitive<T>,
     {
         FresnelZoneIter {
-            _zone: self._zone,
+            zone: self.zone.as_(),
             wavelength: self.wavelength,
+            distance_m: self.distance_m,
             range: 0..len,
         }
     }
@@ -45,38 +47,60 @@ impl<T> FresnelZone<T> {
 #[derive(Debug)]
 pub struct FresnelZoneIter<T> {
     /// Which fresnel zone we're interested in.
-    _zone: NonZeroU8,
+    zone: T,
     wavelength: T,
     range: Range<usize>,
+    distance_m: T,
 }
 
-impl<T: Float + 'static> Iterator for FresnelZoneIter<T>
+impl<T> Iterator for FresnelZoneIter<T>
 where
+    T: Float + 'static,
     usize: AsPrimitive<T>,
 {
     type Item = T;
+
+    #[inline]
     fn next(&mut self) -> Option<T> {
-        self.range.next().map(|n| n.as_() * self.wavelength)
+        self.range.next().map(|n| {
+            let d1 = self.distance_m * (n.as_() / (self.range.end - 1).as_());
+            let d2 = self.distance_m - d1;
+            (self.zone * self.wavelength * d1 * d2 / (self.distance_m)).sqrt()
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{FresnelZone, NonZeroU8};
-
-    #[test]
-    fn test_fresnel_zone_iter() {
-        let iter = FresnelZone::new(NonZeroU8::new(1).unwrap(), 1.0, 10e3).iter(5);
-        let expected = &[0., 1., 2., 3., 4.];
-        assert!(iter.zip(expected.iter()).all(|(l, &e)| {
-            debug_assert_eq!(l, e);
-            l == e
-        }));
-    }
+    use super::FresnelZone;
 
     #[test]
     fn test_zero_len_fresnel_zone_iter() {
-        let mut iter = FresnelZone::new(NonZeroU8::new(1).unwrap(), 1.0, 10e3).iter(0);
+        let mut iter = FresnelZone::new(1, 1.0, 10e3).iter(0);
         assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn test_1st_fresnel_zone() {
+        let mut iter = FresnelZone::new(1, 900e6, 1e3).iter(3);
+        assert_eq!(iter.next(), Some(0.0));
+        assert_eq!(iter.next(), Some(9.125551094469735));
+        assert_eq!(iter.next(), Some(0.0));
+    }
+
+    #[test]
+    fn test_2nd_fresnel_zone() {
+        let mut iter = FresnelZone::new(2, 900e6, 1e3).iter(3);
+        assert_eq!(iter.next(), Some(0.0));
+        assert_eq!(iter.next(), Some(12.90547812192774));
+        assert_eq!(iter.next(), Some(0.0));
+    }
+
+    #[test]
+    fn test_3rd_fresnel_zone() {
+        let mut iter = FresnelZone::new(3, 900e6, 1e3).iter(3);
+        assert_eq!(iter.next(), Some(0.0));
+        assert_eq!(iter.next(), Some(15.805918142687355));
+        assert_eq!(iter.next(), Some(0.0));
     }
 }
