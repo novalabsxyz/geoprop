d76ef02f0505932b45b01ee4901962a6dab5aeb3
Jay Kickliter <jay@kickliter.com>
2023-11-07 16:00:50 -0700

    Log seek time

--- hexit/src/lookup.rs
+++ hexit/src/lookup.rs
@@ -11,11 +11,15 @@ impl Lookup {
             .or_else(|_| u64::from_str_radix(&self.cell, 16))?;
         let cell = Cell::try_from(raw_cell)?;
         let mut disktree = DiskTree::open(&self.disktree)?;
+        let t0 = std::time::Instant::now();
         match disktree.seek_to_cell(cell)? {
             None => (),
             Some((cell, rdr)) => {
+                let t_seek = t0.elapsed();
                 let val = rdr.read_i16::<LE>()?;
+                let t_tot = t0.elapsed();
                 println!("{cell}: {val}");
+                println!("{t_seek:?} {t_tot:?}");
             }
         }
         Ok(())
